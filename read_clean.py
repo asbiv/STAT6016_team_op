72a07e44c37435218ad4cbbd4d73ea326f19032a
asbiv <Ab@Abneys-MacBook-Air.local>
2019-03-25 18:49:47 -0400

    First commit

---
+++ read_clean.py
@@ -0,0 +1,20 @@
+#LOAD LIBRARIES
+import pandas as pd
+import numpy as np
+import os
+
+
+#SET LOCAL PATH
+os.chdir('/Users/Ab/Desktop/SYS6016_Local/team_op/phase_2')
+
+
+#READ IN TRAIN AND TEST
+
+
+
+
+
+
+
+
+
