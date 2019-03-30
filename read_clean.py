aebca23057164c9d45f8808a9b7336cf18749a09
Steve Mortensen <smortensen@outlook.com>
2019-03-30 17:19:43 -0400

    Added keywords and rules, substituted digits with D

--- read_clean.py
+++ read_clean.py
@@ -20,14 +20,9 @@ with urllib.request.urlopen(train_path) as url:
 with urllib.request.urlopen(test_path) as url:
     test_raw = json.loads(url.read().decode())
 
-
 #As dfs
 train_df = pd.read_json(train_path)
 test_df = pd.read_json(test_path)
-
-train_df.dtypes
-
-test_raw
 
 
 #REBUILT DATA
@@ -44,8 +39,11 @@ test_rebuild_df = pd.read_json(test_rebuild)
 train_df = train_rebuild_df.dropna().reset_index()
 test_df = test_rebuild_df.dropna().reset_index()
 
-
+train_df.columns
+train_df['idx'].head()
+train_df['id'].head()
 #PREPROCESSING
+
 
 #REMOVE STOP WORDS
 import nltk
@@ -63,6 +61,10 @@ def remove_stopwords(s):
      return cleanup
 
 train_rm_stop = train_df['tweet'].map(lambda x: remove_stopwords(x))
+
+
+# SUBSTITUTE ALL DIGITS WITH THE LETTER D
+train_digits = train_rm_stop.map(lambda x: re.sub('\d', 'D', x))
 
 
 #TOLKENIZATION OF TWITTER ARTIFACTS
@@ -95,10 +97,13 @@ def preprocess(s, lowercase=False):
 
 #Map preprocess
 train_token = train_rm_stop.map(lambda x: preprocess(x))
+train_token_d = train_digits.map(lambda x: preprocess(x))
+
 
 #CURRENT STATUS...
-print(train_df['tweet'][1])
-print(train_token[1])
+# print(train_df['tweet'][1])
+# print(train_token[1])
+
 
 # LEMMATIZATION
 from nltk.stem import WordNetLemmatizer
@@ -117,7 +122,48 @@ def lemma_loop(s):
 
 # Map lemmatizing functions
 train_lemma = train_token.map(lambda x: lemma_loop(x))
-print(train_token[1], '\n', train_df['tweet'][1])
+train_lemma_d = train_token_d.map(lambda x: lemma_loop(x))
+
+# Summarize lengths of tweet documents
+doc_lengths = list()
+train_lemma.map(lambda x: doc_lengths.append(len(x)))
+doc_lengths[1]
+print('Minimum length of lemmatized tweet: ',min(doc_lengths),'\n','Average: ',sum(doc_lengths)/len(doc_lengths),'\n','Maximum: ',max(doc_lengths))
+
+print(len(train_lemma[1]))
+
+
+# KEYWORDS AND RULES
+keys = {"key_p": ["%","percent","pc","pct"],
+        "key_r": ["up","down","decline","increase","growth","gain","lose","+","-"],
+        "key_m": ["january","jan","february","feb","march","mar","april","apr","may","june","jun","july","jul","august","aug","september","sept","sep","october","oct","november","nov","december","dec"],
+        "key_i": ["ma","dma","sma","ema","rsi","ichimoku"],
+        "key_d": ["day","week","month","year","mos","yrs"],
+        "key_t": ["second","sec","minute","min","mins","hour","hr","hrs","p.m.","pm","a.m."]}
+
+keynames = ["key_p","key_r","key_m","key_i","key_d","key_t", "maturity","date"]
+
+key_vars = pd.DataFrame([], columns=keynames)
+
+nltk.pos_tag(train_lemma[1])
+nltk.pos_tag(train_lemma_d[1])
+
+def key_loop(s):
+    global key_vars
+    key_counts = pd.DataFrame(np.zeros((1,6)),columns=keynames)
+    for token in s:
+        keywords(token, key_counts)
+    key_vars = key_vars.append(key_counts.sum(), ignore_index=True)
+    # return pd.DataFrame(key_counts.sum())
+
+def keywords(token, df):
+    for keylist in keys:
+        if token in keys[keylist] and df[keylist][0] < 1:
+            df[keylist][0] = 1
+    return df
+
+train_lemma.map(lambda x: key_loop(x))
+
 
 # N-GRAM BAG OF WORDS
 from sklearn.feature_extraction.text import CountVectorizer
@@ -130,10 +176,10 @@ def vectorize(s):
 
 # Map bag of words function, join with lemmatized text
 train_bow = pd.DataFrame(vectorize(train_lemma))
-train_bow.shape
+# train_bow.shape
 train_bow['text'] = train_bow.index.map(train_lemma)
 
-print(train_bow.shape)
+# print(train_bow.shape)
 
 # TF-IDF 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -145,6 +191,6 @@ def tf_idf(s):
     return tfidf_vectorizer.fit_transform(s_untokenized).toarray()
 
 train_tfidf = pd.DataFrame(tf_idf(train_lemma))
-print(train_tfidf.shape)
+# print(train_tfidf.shape)
 train_tfidf['text'] = train_tfidf.index.map(train_lemma)
-print(train_tfidf.shape)
+# print(train_tfidf.shape)
