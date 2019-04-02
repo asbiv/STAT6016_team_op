9e0cf5890ff5797db2557a2763eb70fb0eb9296e
asbiv <Ab@Abneys-MacBook-Air.local>
2019-04-02 16:31:23 -0400

    To be reconciled

--- read_clean.py
+++ read_clean.py
@@ -4,32 +4,9 @@ import numpy as np
 import re
 
 
-#SET PATHS
-'''
-import json
-import urllib.request
-
-train_path = 'https://raw.githubusercontent.com/asbiv/STAT6016_team_op/master/data/FinNum_training.json'
-test_path = 'https://raw.githubusercontent.com/asbiv/STAT6016_team_op/master/data/FinNum_test.json'
-dev_path = 'https://raw.githubusercontent.com/asbiv/STAT6016_team_op/master/data/FinNum_dev.json'
-
-
-#READ IN TRAIN AND TEST
-#Train
-with urllib.request.urlopen(train_path) as url:
-    train_raw = json.loads(url.read().decode())
-
-#Test
-with urllib.request.urlopen(test_path) as url:
-    test_raw = json.loads(url.read().decode())
-
-#As dfs
-train_df = pd.read_json(train_path)
-test_df = pd.read_json(test_path)
-'''
-
-
-#REBUILT DATA
+###########################
+###  1. LOAD DATA    ###
+###########################
 #SET PATHS
 train_rebuild = 'https://raw.githubusercontent.com/asbiv/STAT6016_team_op/master/data/FinNum_training_rebuilded.json'
 test_rebuild = 'https://raw.githubusercontent.com/asbiv/STAT6016_team_op/master/data/FinNum_test_rebuilded.json'
@@ -45,6 +22,10 @@ test_df = test_rebuild_df.dropna().reset_index()
 
 
 #PREPROCESSING
+
+###########################
+###  2. EXPAND TWEETS    ###
+###########################
 # EXPAND TWEETS WITH >1 TARGET
 
 # Note: I recognize this could be broken up into different functions, but I'm tired so I'll just comment it a lot instead
@@ -85,6 +66,9 @@ dupe_df['subcategory'] = dupe_df['subcategory'].map(lambda x: x[0])
 dupe_df = dupe_df.append(hold_df).reset_index(drop=True)
 
 
+###############################
+###  3. REMOVE STOPWORDS    ###
+###############################
 #REMOVE STOP WORDS
 import nltk
 from nltk.corpus import stopwords
@@ -104,7 +88,10 @@ def remove_stopwords(s):
 #Active df below
 train_rm_stop_dupe = dupe_df['tweet'].map(lambda x: remove_stopwords(x))
 
-##MAKE SUBSTITUTIONS
+
+#################################
+###  4. MAKE SUBSTITUTIONS    ###
+#################################
 train_copy = train_rm_stop_dupe.copy()
 
 #Remove emojis
@@ -129,6 +116,9 @@ train_lower = train_cash.map(lambda x: x.lower())
 train_d = train_lower.map(lambda x: re.sub('\d', 'D', x))
 
 
+#########################################
+###  5. TOKENIZE TWITTER ARTIFACTS    ###
+#########################################
 #TOKENIZATION OF TWITTER ARTIFACTS
 #Building off this: https://marcobonzanini.com/2015/03/09/mining-twitter-data-with-python-part-2/
 nltk.download('punkt')
@@ -158,13 +148,11 @@ def find_twitter_tokens(s, lowercase=False):
 
 #Map preprocess
 train_token = train_rm_stop_dupe.map(lambda x: find_twitter_tokens(x))
-#train_token_d = train_d.map(lambda x: find_twitter_tokens(x))
-
-#CURRENT STATUS...
-# print(train_df['tweet'][1])
-# print(train_token[1])
 
 
+#########################################
+###  6. LEMMATIZE                     ###
+#########################################
 # LEMMATIZATION
 from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
@@ -182,22 +170,28 @@ def lemma_loop(s):
 
 # Map lemmatizing functions
 train_lemma = train_token.map(lambda x: lemma_loop(x))
-#train_lemma_d = train_token_d.map(lambda x: lemma_loop(x))
 
+#DELETE??
 # Summarize lengths of tweet documents
-doc_lengths = list()
-train_lemma.map(lambda x: doc_lengths.append(len(x)))
-print('Minimum length of lemmatized tweet: ',min(doc_lengths),'\n','Average: ',sum(doc_lengths)/len(doc_lengths),'\n','Maximum: ',max(doc_lengths))
+#doc_lengths = list()
+#train_lemma.map(lambda x: doc_lengths.append(len(x)))
+#print('Minimum length of lemmatized tweet: ',min(doc_lengths),'\n','Average: ',sum(doc_lengths)/len(doc_lengths),'\n','Maximum: ',max(doc_lengths))
 
+
+#########################################
+###  7. TAG PARTS OF SPEECH           ###
+#########################################
 
 # POS_TAGGING
 nltk.download('averaged_perceptron_tagger')
 train_pos = train_lemma.map(lambda x: nltk.pos_tag(x))
-train_pos.head()
 
 
+
+#########################################
+###  8. ENCODE CHARACTERS             ###
+#########################################
 # ENCODE ALL CHARACTERS
-
 # Runs through all the basic unicode characters, creates a range, assigns 0's for everything except for the index of the character
 def onehot_char(char):
     return [1 if i==ord(char) else 0 for i in range(32,126)]
@@ -219,7 +213,6 @@ char_size = max(char_enc_list.map(len))
 char_size
 
 #Add lists of 0s to make sublists len of char_size
-import copy
 char_enc_list_padded = char_enc_list.copy()
 
 #WARNING: Long run time (~5 seconds)
@@ -232,13 +225,14 @@ def pad_char_enc(l):
     return(l)
 
 char_enc_list_padded = pad_char_enc(char_enc_list_padded)
-char_enc_list_padded[0]
 
-# INDEX LOCATION OF TARGET
 
-# INDEX TOKEN LOCATION OF TARGET
+############################################
+###  9. GET INDEX LOCATION OF TARGETS    ###
+############################################
+
 # NOTE: Ignore this token indexing - use the character index below
-
+'''
 def index_target(s):
     # Call out global variables
     i = list(train_lemma.index(s))
@@ -266,6 +260,7 @@ tgt_loc = pd.get_dummies(pd.Series(target_index))
 
 # Again, :( 23 errors
 target_index.count(-1)
+'''
 
 # INDEX CHARACTER LOCATION OF TARGET
 def index_target(itarget):
@@ -305,7 +300,9 @@ for x in colpad:
     tgt_loc_char[x] = 0
 
 
-# STACK EVERYTHING FOR CNN INPUT
+##############################################
+###  10. STACK EVERYTHING FOR CNN INPUT    ###
+##############################################
 
 # Stack character encoding lists of lists into individual dataframes and transpose them
 # NOTE: The numbering in the print statements is weird here - disregard it, 
